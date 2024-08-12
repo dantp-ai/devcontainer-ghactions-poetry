4bcda23db7d5eb1104827a550c460e1464125cdb
daniel <1534513+dantp-ai@users.noreply.github.com>
2024-08-12 20:44:05 +0200

    Add some new test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,6 +6,17 @@ def test_one() -> None:
     assert len([1, 3, 4]) == 3
 
 
+@pytest.mark.parametrize(
+    "inp, expected",
+    [
+        (1, 2),
+        (3, 4),
+    ],
+)
+def test_add(inp: int, expected: int) -> None:
+    assert inp + 1 == expected
+
+
 def test_two() -> None:
     with pytest.raises(KeyError):
         d: dict[str, Any] = {}
